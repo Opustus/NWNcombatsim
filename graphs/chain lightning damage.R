3125c3e5859d8ec4acdf91843bb3f25c4ae86f8d
Mitro Hyvärinen <mitro.hyvarinen@gmail.com>
2022-08-24 11:31:36 +0300

    Update chain lightning damage.R

--- graphs/chain lightning damage.R
+++ graphs/chain lightning damage.R
@@ -1,51 +1,45 @@
 ## Old chain lightning
 
-
-old_chain = function(targets)
-{
-  if (targets > 2)
-  {
-    dmg = 120 + 60*6 + (targets-2)*7*30+30
+oldchain = c()
+for(targets in 1:20){
+  dmg_out <- 120 + 60*6 + (targets-2)*7*30+30
+  oldchain <- c(oldchain, dmg_out)
+  if (targets == 1){
+    oldchain[targets] <- 120  
   }
-  if (targets == 2)
-  {
-    dmg = 120 + 60*6 + 30
-  }
-  if (targets == 1)
-  {
-    dmg = 120
-  }
-  return(dmg)
 }
-old_chain(10)
+oldchain_othertarget = round((oldchain-120)/c(1:20),0)
+oldchain_othertarget
+oldchain_firsttarget = oldchain_othertarget+120
+oldchain_firsttarget
 
-
-targets <- 0
-x <- 0
-dmg <- c()
-
-new_chain = repeat{
-  targets <- targets+1
-  x = 120 * targets
-  dmg[x] <- x
-  print(dmg)
-  
-  if (targets > 10) break
-}
-
-  
-  
-  
-
-new_chain = function(targets)
-{
-  if (targets == 1)
-  {
-    dmg = 120
+newchain = c()
+for(targets in 1:20){
+  dmg_out <- 120 * 6
+  newchain <- c(newchain, dmg_out)
+  if (targets == 1){
+    newchain[targets] <- 120
   }
-  else
-  {
-    dmg = 120 * 6
-  }
-  return(dmg)
 }
+newchain
+
+newchain_target = newchain/c(1:20)
+newchain_target[7:20] <- 0
+newchain_target
+targets
+
+plot(oldchain, type = "l")
+lines(oldchain_firsttarget, col = "blue")
+lines(oldchain_othertarget, col = "red")
+legend(1, 3900, legend=(c("Overall damage","First target", "Other targets")), col=c("black","blue","red"), lty=1, cex = 0.75)
+
+?plot()
+plot(x=1:20, oldchain, type = "l", ylim = c(0,500))
+lines(oldchain_firsttarget, col = "blue")
+lines(oldchain_othertarget[2:20], col = "red")
+legend(11, 480, legend=(c("Overall damage","First target", "Other targets")), col=c("black","blue","red"), lty=1, cex = 0.75)
+
+plot(newchain, type = "l", ylim = c(0,720))
+lines(newchain_target, col = "blue")
+legend(11, 480, legend=(c("Overall damage","Targets")), col=c("black","blue","red"), lty=1, cex = 0.75)
+
