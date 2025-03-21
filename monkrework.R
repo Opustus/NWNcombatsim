ec7032fefa453dee77f5fd0980e808a2435fa940
Mitro Stönky <90032996+Opustus@users.noreply.github.com>
2025-03-21 09:19:59 +0200

    Do something....

--- monkrework.R
+++ monkrework.R
@@ -1,6 +1,6 @@
 roll20 = 1:20
 
-attackSequence = function(ab, baseApr, ubab = F)
+attacks = function(ab, baseApr, ubab = F)
 {
 	firstAttackAb = ab 
 	attackDecrement = -5
@@ -12,6 +12,28 @@ attackSequence = function(ab, baseApr, ubab = F)
 
 	lastAttackAb = firstAttackAb - attackDecrement * (baseApr-1)
 	seq(from = firstAttackAb, to = lastAttackAb, by = attackDecrement)
+}
+
+attacks_dataframe = function(ab, baseApr, ubab = F, haste = T, flurry = F)
+{
+	dataframe <- data.frame()
+	
+	for (i in 1:baseApr)
+	{
+		dataframe <- cbind(attacks(ab, baseApr, ubab)[i]+roll20)
+	}
+  
+	if (flurry == T || haste == T)
+	{
+    		mat = cbind(c1,c1,c2,c3,c4)
+	}
+	
+  	if (haste == T && rapid == T)
+  	{
+    		mat = cbind(c1,c1,c1,c2,c3,c4)
+  	}
+	
+  	return(mat)
 }
 
 
